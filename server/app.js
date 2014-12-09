54e601543c1276ee029722f6805bede6c470a029
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-12-09 11:33:58 +0530

    added full text support

--- server/app.js
+++ server/app.js
@@ -1,5 +1,9 @@
 Meteor.publish('getEntries', function(searchText) {
-  return Entries.find({}, {sort: {name: 1}, limit:20});
+  if(searchText) {
+    return Entries.find({$text: {$search: searchText}}, {limit: 20});
+  } else {
+    return Entries.find({}, {sort: {name: 1}, limit:20});
+  }
 });
 
 Meteor.methods({
