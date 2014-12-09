536c10f88564f9d7a40739a4183af9c05b765523
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-12-09 12:05:36 +0530

    removed normal find

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ Meteor.publish('getEntries', function(searchText) {
   if(searchText) {
     return Entries.find({$text: {$search: searchText}}, {limit: 20});
   } else {
-    return Entries.find({}, {sort: {name: 1}, limit:20});
+    this.ready();
   }
 });
 
